4bcd355d471d8eb440e706ad748bf941cf1a5e02
Alexandr-Mayyura <sonickill@yandex.ru>
2023-03-06 13:34:56 +0300

    Initial Commit

---
+++ GenerationImage/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GenerationImage
+//
+//  Created by Aleksandr Mayyura on 06.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GenerationImage/GenerationImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerationImageApp.swift
+//  GenerationImage
+//
+//  Created by Aleksandr Mayyura on 06.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct GenerationImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
